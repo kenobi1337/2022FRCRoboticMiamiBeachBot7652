d3213905e7eff83381914af5a233007ec586fd03
Chanathip Gulgalkhai <kenobi1337@protonmail.com>
2022-01-04 21:49:05 -0500

    Added some descritions to DriveTrainSubsystems methods

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -16,35 +16,38 @@ import edu.wpi.first.wpilibj.interfaces.Gyro;
 import com.analog.adis16448.frc.ADIS16448_IMU;
 
 
-/** An example subsystem. You can replace with me with your own subsystem. */
 public class DriveTrainSubsystem extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
 
+  // declare properties for DriveTrainSubsystem
   public static DriveTrainSubsystem m_drive;
   private double rspeed = 0;
   private double lspeed = 0;
   private final double delta = 0.05;
   private final double maxSpeed = 0.8;
+  private double kP = 0.0075;
 
-  double kP = 0.0075;
 
-
+  // declare motors
   Victor m_frontRight = new Victor(1); //added motor 1 and 2 but we should group them in speed controller groups and call them right and left side them put them into differnetial drive
   Victor m_backLeft = new Victor(2);
   Victor m_backRight = new Victor(3);
   Victor m_frontLeft = new Victor(4);
 
+  // declare motors groups
   SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_backLeft);
   SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_backRight);
 
 
   DifferentialDrive m_ddrive = new DifferentialDrive(m_left, m_right);
 
-  public DriveTrainSubsystem() {
+  // default constructor
+  public DriveTrainSubsystem() {}
 
-  }
-
+  /**
+   * @Description: Static method to get an instance of DriveTrainSubsystems
+   *
+   * @return DriveTrainSubsystem instance
+   */
   public static DriveTrainSubsystem getInstance(){
     if (m_drive == null){
       m_drive = new DriveTrainSubsystem();
@@ -52,10 +55,23 @@ public class DriveTrainSubsystem extends Subsystem {
     return m_drive;
   }
 
+  /**
+   * @Description: Method to drive the robot using controlled speed from the user input(Joystick)
+   *
+   * @param leftSpeed
+   * @param rightSpeed
+   */
   public void tankDrive(double leftSpeed, double rightSpeed) {
     m_ddrive.tankDrive(leftSpeed, rightSpeed);
   }
 
+
+  /**
+   * @Description: Will ask Diego further more about this method
+   *
+   * @param leftTarget
+   * @param rightTarget
+   */
   public void trapDrive(double leftTarget, double rightTarget) {
     double leftSpeed, rightSpeed;
 
@@ -83,6 +99,12 @@ public class DriveTrainSubsystem extends Subsystem {
     m_ddrive.tankDrive(leftSpeed,rightSpeed);
   }
 
+  /**
+   * @Description: Will ask Diego further more about this method
+   *
+   * @param leftTarget
+   * @param rightTarget
+   */
   public void gyroTrapDrive(double leftTarget, double rightTarget) {
     // double error = 0;
     double error = -getGyro().getRate();
@@ -111,6 +133,12 @@ public class DriveTrainSubsystem extends Subsystem {
     m_ddrive.tankDrive(leftSpeed + kP * error,rightSpeed - kP * error);
   }
 
+  /**
+   * Description: Will ask Diego further more about this method
+   *
+   * @param leftSpeed
+   * @param rightSpeed
+   */
   public void gyroDrive(double leftSpeed ,double rightSpeed) {
     double error = -getGyro().getRate();
     String str = Double.toString(error);
@@ -118,6 +146,9 @@ public class DriveTrainSubsystem extends Subsystem {
     m_ddrive.tankDrive(leftSpeed + kP * error, rightSpeed - kP * error);
   }
 
+  /**
+   * Description: Method to utilize gyro in turning functionality
+   */
   public void gyroTurnFunc() {
     double error = 90 - getGyro().getAngle();
     String str = Double.toString(error);
@@ -125,6 +156,11 @@ public class DriveTrainSubsystem extends Subsystem {
     m_drive.tankDrive(kP * error, kP * -error);
   }
 
+  /**
+   * @Description: Static method to return gyro instance
+   *
+   * @return gyro instance
+   */
   public static ADIS16448_IMU getGyro() {
     ADIS16448_IMU gyro = new ADIS16448_IMU();
     return gyro;
